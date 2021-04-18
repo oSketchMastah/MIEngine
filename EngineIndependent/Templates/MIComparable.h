72a5c7863198223ad4ac0fcfcdcf23eb10b89c98
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-04-18 19:23:44 -0400

    Add files via upload

--- EngineIndependent/Templates/MIComparable.h
+++ EngineIndependent/Templates/MIComparable.h
@@ -2,16 +2,16 @@
 /*give a class type, and a basic type, using CRTP this signs up this class
 to be comparable amongst itself using a type T to which it delegates for comparisons*/
 namespace MI {
-	template<typename Class, typename T>
+	template<typename T>
 	struct Comparable {
 		Comparable(T c) : obj{ c } {}
 		Comparable(const Comparable& other) { obj = other.obj; }
 		~Comparable() {}
 		operator T() { return obj; }
-		Comparable operator< (const Comparable& other) { return obj < other.obj; }
-		Comparable operator> (const Comparable& other) { return obj > other.obj; }
-		Comparable operator== (const Comparable& other) { return obj == other.obj; }
-		Comparable operator!= (const Comparable& other) { return obj != other.obj; }
+		Comparable operator< (const T& other) { return obj < other; }
+		Comparable operator> (const T& other) { return obj > other; }
+		Comparable operator== (const T& other) { return obj == other; }
+		Comparable operator!= (const T& other) { return obj != other; }
 	private:
 		T obj;
 	};

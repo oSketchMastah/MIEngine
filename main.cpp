49de587d6e6077f0c81392d2a6116110a840579c
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-05-05 19:41:06 -0400

    cleanup
    cleaning some more experimental tests from main.cpp

--- main.cpp
+++ main.cpp
@@ -28,22 +28,7 @@ constexpr size_t BitV() {
  
 template <int bitnum>
 struct Bitcalc {
-
 #define CheckBit(id) if constexpr ((bitnum & BitV<id>()) != 0) { return id; }
-//#define CheckBits(_0, ...) CheckBit(_0) else CheckBit( __VA_ARGS__)
-
-	/*template<int id>
-	constexpr bool CheckB() {
-		if constexpr ((bitnum & BitV<id>()) != 0) { return true; }
-		return false;
-	}
-
-	template <int num1, int ... nums>
-	constexpr int CheckBits(int) {
-		if constexpr CheckB(num1) { return };
-		return CheckBits(nums...);
-	}*/
-
 
 	constexpr int first_on_bit_id() {
 		//return CheckBits<0, 1, 2, 3, 4, 5, 6, 7, 8>();
@@ -60,106 +45,7 @@ struct Bitcalc {
 };
 // BITCALC
 
-/*
-struct Convolution;
-
-struct VirtualExpression {
-
-public:
-	VirtualExpression();
-	~VirtualExpression();
-
-	virtual VirtualExpression& Morph(const Convolution& conv, VirtualExpression subexpr);
-	virtual VirtualExpression& Evaluate();
-	virtual void Display();
-};
-
-//helpers for testing operators
-struct Null {};
-//template<typename T, typename Arg> Null operator== (const T&, const Arg&);
-template<typename T, typename Arg> Null operator!= (const T&, const Arg&);
-*//*
-
 #include "MIDetectionTests.h"
-constexpr size_t hashBase = 27;//only count letters
-constexpr size_t hashTableSize = 3000;
-//returns a numeric representation for a unique character,
-//  0 for no character, [1, 26] for letters in english alphabet
-size_t HashChar(char c) {
-	if ('a' <= c && c <= 'z') {
-		return c - 'a' + 1;
-	}
-	else if ('A' <= c && c <= 'Z') {
-		return c - 'A' + 1;
-	}
-	//The hashing of letter might need changing if you rely on lower/uppercase differences
-	return 0;
-}
-size_t ClassHash(const char * name) {
-	long long int hashv = 0;
-	for (;	*name != '\0' ; name++) {
-		if ('a' <= *name && *name <= 'z' || 'A' <= *name && *name <= 'Z') {
-			hashv = hashBase * hashv + HashChar(*name);
-		}
-	}
-	return hashv % hashTableSize;
-}
-namespace MI {
-	struct MetaData {
-		String name;
-		MetaData() : name() {}
-		MetaData(const String& str) : name(str) {}
-		~MetaData() {}
-		bool Initialized() { return name.Size() == 0; }
-	};
-}
-struct Base {
-
-	static inline Array<MetaData*, hashTableSize> classnames;
-	int hashv;
-
-	void Test() {
-		printf("Base for %s called%s", classnames[, "\n");
-	}
-	template <typename Class>
-	static void Initialize() {
-		//hash the name into the static classname list, 
-		int hash = ClassHash(Class::MetaData::name);
-		if (!classnames[hash].Initialized()) {
-			DoAssertMsg("Hash collision detected");
-		}
-		classnames.Insert( Class::MetaData , hash);
-	}
-};
-
-struct A : public Base {
-	static struct MetaData {
-		const char* name = "B";
-	};
-
-	static void Initialize() {
-		Base::Initialize<A>();
-	}
-	void Test() {
-		Base::Test();
-		printf("%s%s", "A called", "\n");
-	}
-};
-
-struct B : public A, public Base {
-	static struct MetaData {
-		const char* name = "B";
-	};
-
-	static void Initialize() {
-		Base::Initialize<B>();
-	}
-	void Test() {
-		Base::Test();
-		A::Test();
-		printf("%s%s", "B called", "\n");
-	}
-};*/
 
 
 bool greaterThan9(int k) { return k >= 10; };
@@ -251,25 +137,18 @@ int main(int argc, char* argv[]){
 	printf("%s%s", mf.Read().c_str(), "\n");
 	mf.Close();
 	
-	/*String a = "a";
+	String a = "a";
 	String b = "b";
 	String abc = "abc" ;
 	ComparisonTester<String> stringcomptest;
 	Assert(stringcomptest.Test(a, b, Assertion::Less));
-*/
 
-	/*LogicSequence<int, 10> ls;
+	LogicSequence<int, 10> ls;
 	ls.StartTrue(1);
 	ls.AndNew(7);
 	ls.AndNewNot(9); //FALSE
 	ls.OrNewNot(13);
 	std::cout << (std::boolalpha) << ls.Evaluate(lessThan10, greaterThan9) << std::endl;
 
-*/
-	//A aex;
-	/*B bex;
-	B::Initialize();
-	//aex.Test();
-	bex.Test();*/
 	return 0;
 }

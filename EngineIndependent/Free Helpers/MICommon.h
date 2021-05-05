42ed2d8176daf21da9bcd7291ab0bdc2df1fb4ae
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-05-05 19:27:33 -0400

    Delete EngineIndependent directory

--- EngineIndependent/Free Helpers/MICommon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "String.h"
-#include "EngineLogger.h"
-#include "Vector.h"
-
-namespace MI {
-	template <typename T>
-	using Vector = Vector<T>;
-}
--- EngineIndependent/Free Helpers/MIStringUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace MI {
-	//get the length of a cstring
-	int strlen(const char* str);
-	//unchecked copy of src string to dest string
-	void strcpy(char* dest, int len, const char* src);
-}

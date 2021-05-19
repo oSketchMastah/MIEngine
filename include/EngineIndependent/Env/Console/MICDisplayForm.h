823bef642701038946f7e5fa161e0c4d6e3eaa76
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-05-19 07:46:49 -0400

    Delete MICDisplayForm.h

--- include/EngineIndependent/Env/Console/MICDisplayForm.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "MIUniquePtr.h"
-#include "MIBank.h"//probably will need something more specific to a single terminal
-struct Bank;
-struct DisplayForm {
-	Bank& displaybank;
-};

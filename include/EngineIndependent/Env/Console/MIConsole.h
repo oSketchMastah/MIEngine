482592f4ffac16ddaf3a8faa202467bef0009a83
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-05-29 04:29:46 -0400

    Update MIConsole.h

--- include/EngineIndependent/Env/Console/MIConsole.h
+++ include/EngineIndependent/Env/Console/MIConsole.h
@@ -14,18 +14,23 @@ struct ConsoleInput;
 struct Console {
 	Console(){}
 	~Console();
-	//A method for getting access to different components of the console
+	/*A method for getting access to different components of the console (Ex. include "MIConsoleOutput.h", then you can use Get<ConsoleOutput>())
+	*types accessable using Get (specialized for in implementation), and their include file:
+	*	- Get<ConsoleOutput>()	 -	MIConsoleOutput.h
+	*	- Get<ConsoleInput>()	 -	MIConsoleInput.h
+	*	- Get<ConsoleSettings>() -	MIConsoleSettings.h
+	*/
 	template <typename T>
 	T& Get();
-	//ConsoleOutput&	Get<ConsoleOutput>();
-	//ConsoleInput&	Get<ConsoleInput>();
-
-	//Sets up the console, might include installing our own buffer in the current terminal, setting up virtual sequence interpretation (in windows)
-	#ifdef WINDOWS
-		static int Initialize(const int inbufsize = -1, const int outbufsize = 16385);
-	#else //Everything else probably does a good job with default output buffer (-1 means do nothing)
-		static int Initialize(const int inbufsize = -1, const int outbufsize = -1);
-	#endif
+	
+	
+	/*Sets up the console, might include installing our own buffer in the current terminal, setting up virtual sequence interpretation (in windows)
+	*   Note: Windows sets the arbitrary limit for outbufsize to 16385 (2^14), which makes writing to console significantly faster on any OS.
+	*   In non-Windows OS, increasing beyond this might be worth it if you are doing advanced console animations, or are just doing 
+	*	a lot of console writing and don't mind using the extra memory. You can flush stdout to see anything written by an application.
+	*   IMPORTANT: this will modify the underlying console (running the app) with its effects until another call to Initialize or setvbuf.
+	*/
+	static int Initialize(const int inbufsize = -1, const int outbufsize = 16385);
 private:
 	struct Impl;
 	UniquePtr<Impl> pImpl;
